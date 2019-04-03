87b91f4b3cd6ccc37505d01446cc967add0938c9
Evandro Carreira <evandro.carreira@gmail.com>
2019-04-03 14:15:49 -0300

    Menu simples adicionado ao topo da home

---
+++ src/components/SimpleMenu.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import TouchableHighlight from '../ui/TouchableHighlight';
+import View from '../ui/View';
+import { IoMdQrScanner } from 'react-icons/io';
+import { FaStar, FaHeart, FaWhatsapp } from 'react-icons/fa';
+import { Link } from 'react-router-dom'
+
+class SimpleMenu extends Component {
+
+  render() {
+    return <View style={this.style.container}>
+      <Link
+        to = "/qrcode"
+      >
+        <IoMdQrScanner
+          size={26}
+          style={this.style.icon}
+        />
+      </Link>
+      <Link
+        to = "/meuspontos"
+      >
+        <FaStar
+          size={26}
+          style={this.style.icon}
+        />
+      </Link>
+      <Link
+        to = "/areacliente"
+      >
+        <FaHeart
+          size={26}
+          style={this.style.icon}
+        />
+      </Link>
+      <Link
+        to="/whats"
+      >
+        <FaWhatsapp
+          name="whatsapp"
+          size={26}
+          style={this.style.icon}
+        />
+      </Link>
+    </View>
+  }
+
+  goTo = (page) => {
+    this.props.navigation.navigate(page)
+  }
+
+  style = {
+    container: {
+      borderTopWidth: 2,
+      borderBottomWidth: 2,
+      borderColor: "#bdbbbc",
+      flexDirection: "row",
+      backgroundColor: "#ebebeb",
+      paddingTop: 7,
+      paddingBottom: 8,
+      justifyContent: "space-around"
+    },
+    icon: {
+      color: "#bdbbbc"
+    }
+  }
+}
+
+export default SimpleMenu
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/Header'
 import RodapeCompleto from '../components/RodapeCompleto';
+import SimpleMenu from '../components/SimpleMenu';
 
 class Home extends Component {
   render() {
     return <>
       <Header/>
+      <SimpleMenu/>
       <h4>slider cupons</h4>
       <h4>slider ofertas</h4>
       <RodapeCompleto/>
