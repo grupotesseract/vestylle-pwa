9a622640712607623c3f92eb56be1c5db84978f1
Evandro Carreira <evandro.carreira@gmail.com>
2019-06-10 17:58:47 -0300

    teste de custom sw

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -35,7 +35,8 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/webpush-sw.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
