a0086cb3c62a1ba21503f85e2b6ff09b042299b5
Evandro Carreira <evandro.carreira@gmail.com>
2019-06-12 22:11:29 -0300

    sw simples

--- public/webpush-sw.js
+++ public/webpush-sw.js
@@ -1,16 +1,11 @@
 self.addEventListener('push', function (e) {
     e.waitUntil(
-        self.registration.pushManager.getSubscription()
-        .then(function(subscription) {
-            if (e.data) {
-                var msg = e.data.json();
-                console.log(msg)
-                return self.registration.showNotification(msg.title, {
-                    body: msg.body,
-                    icon: msg.icon,
-                    actions: msg.actions
-                });
-            }
+        var msg = e.data.json();
+        console.log(msg)
+        return self.registration.showNotification(msg.title, {
+            body: msg.body,
+            icon: msg.icon,
+            actions: msg.actions
         });
     );
 });
