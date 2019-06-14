f82ac8f6d1bb7ceff1babdf724cb50a58e174cc5
Evandro Carreira <evandro.carreira@gmail.com>
2019-06-14 10:53:09 -0300

    notification click e fix bug js

--- public/webpush-sw.js
+++ public/webpush-sw.js
@@ -1,16 +1,28 @@
 self.addEventListener('push', function (e) {
     e.waitUntil(
-        e.data.json()
-        .then(msg => {
-            return self.registration.showNotification(msg.title, {
-                body: msg.body,
-                icon: msg.icon,
-                actions: msg.actions
-            });
-        })
+        console.log(e.data);
+        var msg = e.data;
+        return self.registration.showNotification(msg.title, {
+            body: msg.body,
+            icon: msg.icon,
+            actions: msg.actions
+        });
     );
 });
 
 self.addEventListener('notificationclick', function(event) {
-  console.log('notificationclicked:',event);
+    console.log('notification clicked:',event);
+    const rootUrl = new URL('/', location).href;
+    event.notification.close();
+    // Enumerate windows, and call window.focus(), or open a new one.
+    event.waitUntil(
+      clients.matchAll().then(matchedClients => {
+        for (let client of matchedClients) {
+          if (client.url === rootUrl) {
+            return client.focus();
+          }
+        }
+        return clients.openWindow("/");
+      })
+    );
 });
