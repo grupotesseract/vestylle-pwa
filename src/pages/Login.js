10f04881a83c42a61c1a365ecbaaccef3be0d1b4
Evandro Carreira <evandro.carreira@gmail.com>
2019-07-08 17:34:10 -0300

    minHeight: 100% => height: window.innerHeight

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -27,7 +27,7 @@ export default class LoginScreen extends React.Component {
     return (
       <ImageBackground
         source={require('../assets/fundologin.jpg')}
-        style={{width: '100%', minHeight: '100%' }}>
+        style={{width: '100%', height: window.innerHeight }}>
 
         <View 
         className="container container-sm"
